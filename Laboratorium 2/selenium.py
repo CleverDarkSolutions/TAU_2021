dd44880d4cef91bfb0bd5b2af827627014a98b0b
CleverDarkSolutions <66365110+CleverDarkSolutions@users.noreply.github.com>
2023-03-15 18:14:57 +0100

    Lab 2 done

--- Laboratorium 2/selenium.py
+++ Laboratorium 2/selenium.py
@@ -1,23 +1,95 @@
+import unittest
 from selenium import webdriver
-import logging
+from selenium.webdriver.common.keys import Keys
 
-logger = logging.getLogger('simple_example')
-logger.setLevel(logging.INFO)
-ch = logging.StreamHandler()
-ch.setLevel(logging.DEBUG)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-ch.setFormatter(formatter)
-logger.addHandler(ch)
 
-driver = webdriver.Chrome(executable_path='C:\chromedriver.exe')
+class LoginTests(unittest.TestCase):
 
-logger.info('Przechodzę na stronę Zalando')
-driver.get('https://www.zalando.pl/')
-temp = driver.find_element_by_class_name('z-navicat-header_navToolItemLink')
-temp.click()
-logger.warning('Jakieś ostrzeżenie')
-temp = driver.find_element_by_id('login.email')
-temp.send_keys("login")
-logger.error('Jakiś Error')
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+        self.driver.get("https://www.saucedemo.com/")
 
-driver.close()
+    def tearDown(self):
+        self.driver.close()
+
+    def test_valid_login(self):
+        username_field = self.driver.find_element_by_id("user-name")
+        password_field = self.driver.find_element_by_id("password")
+        login_button = self.driver.find_element_by_css_selector(".btn_action")
+
+        username_field.send_keys("standard_user")
+        password_field.send_keys("secret_sauce")
+        login_button.click()
+
+        self.assertTrue("https://www.saucedemo.com/inventory.html" in self.driver.current_url)
+
+    def test_invalid_login(self):
+        username_field = self.driver.find_element_by_id("user-name")
+        password_field = self.driver.find_element_by_id("password")
+        login_button = self.driver.find_element_by_css_selector(".btn_action")
+
+        username_field.send_keys("invalid_user")
+        password_field.send_keys("invalid_password")
+        login_button.click()
+
+        self.assertTrue("https://www.saucedemo.com/" in self.driver.current_url)
+
+    def test_empty_username(self):
+        username_field = self.driver.find_element_by_id("user-name")
+        password_field = self.driver.find_element_by_id("password")
+        login_button = self.driver.find_element_by_css_selector(".btn_action")
+
+        password_field.send_keys("secret_sauce")
+        login_button.click()
+
+        self.assertTrue("https://www.saucedemo.com/" in self.driver.current_url)
+
+    def test_empty_password(self):
+        username_field = self.driver.find_element_by_id("user-name")
+        password_field = self.driver.find_element_by_id("password")
+        login_button = self.driver.find_element_by_css_selector(".btn_action")
+
+        username_field.send_keys("standard_user")
+        login_button.click()
+
+        self.assertTrue("https://www.saucedemo.com/" in self.driver.current_url)
+
+    def test_invalid_username_format(self):
+        username_field = self.driver.find_element_by_id("user-name")
+        password_field = self.driver.find_element_by_id("password")
+        login_button = self.driver.find_element_by_css_selector(".btn_action")
+
+        username_field.send_keys("invalid@user.com")
+        password_field.send_keys("secret_sauce")
+        login_button.click()
+
+        self.assertTrue("https://www.saucedemo.com/" in self.driver.current_url)
+
+    def test_invalid_password_format(self):
+        username_field = self.driver.find_element_by_id("user-name")
+        password_field = self.driver.find_element_by_id("password")
+        login_button = self.driver.find_element_by_css_selector(".btn_action")
+
+        username_field.send_keys("standard_user")
+        password_field.send_keys("123456")
+        login_button.click()
+
+        self.assertTrue("https://www.saucedemo.com/" in self.driver.current_url)
+
+    def test_logout(self):
+        username_field = self.driver.find_element_by_id("user-name")
+        password_field = self.driver.find_element_by_id("password")
+        login_button = self.driver.find_element_by_css_selector(".btn_action")
+
+        username_field.send_keys("standard_user")
+        password_field.send_keys("secret_sauce")
+        login_button.click()
+
+        logout_button = self.driver.find_element_by_id("logout_sidebar_link")
+        logout_button.click()
+
+        self.assertTrue("https://www.saucedemo.com/" in self.driver.current_url)
+
+
+if __name__ == '__main__':
+    unittest.main()
